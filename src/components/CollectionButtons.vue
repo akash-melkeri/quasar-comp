7cbf0290203f869cbc6db06e60a432f51b2c0414
akash-melkeri <akashmelkeri23@gmail.com>
2023-09-11 19:00:32 +0530

    inputs

---
+++ src/components/CollectionButtons.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tw-flex tw-flex-col tw-gap-16">
+    <div class="tw-flex tw-flex-col tw-gap-4">
+      <div>Small Buttons</div>
+      <div class="tw-grid tw-grid-cols-4 tw-gap-4"></div>
+    </div>
+    <div class="tw-flex tw-flex-col tw-gap-4">
+      <div>Medium Buttons</div>
+      <div class="tw-grid tw-grid-cols-4 tw-gap-4"></div>
+    </div>
+    <div class="tw-flex tw-flex-col tw-gap-4">
+      <div>Large Buttons</div>
+      <div class="tw-grid tw-grid-cols-4 tw-gap-4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="">
+    <CollectionButtons
+      v-if="
+        $route.query.component == 'buttons' ||
+        Object.keys($route.query).length == 0
+      "
+    ></CollectionButtons>
+    <CollectionTextInputs
+      v-if="$route.query.component == 'text-inputs'"
+    ></CollectionTextInputs>
+    <CollectionFileInputs
+      v-if="$route.query.component == 'file-inputs'"
+    ></CollectionFileInputs>
+    <CollectionTextarea
+      v-if="$route.query.component == 'textareas'"
+    ></CollectionTextarea>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import CollectionButtons from "src/components/CollectionButtons.vue";
+import CollectionTextInputs from "src/components/CollectionTextInputs.vue";
+import CollectionFileInputs from "src/components/CollectionFileInputs.vue";
+import CollectionTextarea from "src/components/CollectionTextarea.vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  components: {
+    CollectionButtons,
+    CollectionTextInputs,
+    CollectionFileInputs,
+    CollectionTextarea
+  },
+  setup() {
+    return {
+      map: {
+        buttons: {}
+      }
+    };
+  }
+});
 </script>
